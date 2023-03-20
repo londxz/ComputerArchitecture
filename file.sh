0391b92a3bb4131fd43208d07f3a1b03aab038f1
londxz <116108420+londxz@users.noreply.github.com>
2023-03-20 10:13:46 +0300

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
-usage=$(df —output=pcent /media/conk/LOG/ | grep -o '[0-9]*')
-if [ $usage -ge 1 ]
+usagelimit=1
+
+if [ ! -d "/media/conk/LOG" ]
 then
-tar -czvf /media/conk/BACKUP/test.tar.gz /media/conk/LOG
-for file in /media/conk/LOG/*
-do
-rm $file
-done
+	echo "LOG doesn't exit"
+	exit 0
+fi
+
+usage=$(df --output=pcent /media/conk/LOG | grep -o '[0-9]*')
+
+if [ ! -d "/media/conk/BACKUP" ]
+then
+	echo "BACKUP doesn't exit"
+	exit 0
+fi
+
+if [ ! $usage -ge $usagelimit ]
+then
+	echo "LOG usage is less than $usagelimit%"
+	exit 0
+fi
+if [ $usage -ge $usagelimit ]
+then
+	cd /media/conk/LOG
+	list=()
+	for file in $(ls -t | tail -n4)
+	do
+		if [ $file != "lost+found" ]
+		then
+			list+=( $file )
+		fi
+	done
+	
+	if [ ! ${#list[0]} -eq 0 ]
+	then
+		tar -czf /media/conk/BACKUP/archive.tar.gz ${list[@]}
+		for file in /media/conk/LOG/${list[@]}
+		do
+			rm $file
+		done
+	else
+		echo "Not enough file in LOG"
+	fi
+	echo "Script has finished successfully"
 fi
